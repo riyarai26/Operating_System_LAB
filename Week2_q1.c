6c1d154860d3436f798ee733eab4c5d54f436330
Riya rai <rairiya669@gmail.com>
2025-11-06 21:16:24 +0530

    Program to Open a Directory and List Its Contents

---
+++ Week2_q1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <dirent.h>
+int main()
+{
+    DIR *d;
+    struct dirent *dir;
+    d = opendir("."); 
+    if (d == NULL)
+    {
+        printf("Unable to open directory.\n");
+        return 0;
+    }
+    printf("Contents of Directory:\n");
+    while ((dir = readdir(d)) != NULL)
+    {
+        printf("%s\n", dir->d_name);
+    }
+    closedir(d);
+    return 0;
+}
